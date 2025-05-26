5131bc5cacc8bfe2bdc065d66612b8b69e978782
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-05-26 03:32:07 +0300

    Add Global exception handling middleware
    - Added global exception handling middleware to the pipeline.
    - Introduced "ProblemDetail" class for structured error responses.
    - Enhanced "GlobalExceptionMiddleware" to log exceptions and return JSON error details.

---
+++ EduPlatform.Presentation/Helpers/ProblemDetail.cs
@@ -0,0 +1,15 @@
+﻿namespace EduPlatform.Presentation.Helpers
+{
+    public class ProblemDetail
+    {
+        public ProblemDetail(int statusCode, string message = null, string detail = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Detail = detail; 
+        }
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public string? Detail { get; set; }
+    }
+}
