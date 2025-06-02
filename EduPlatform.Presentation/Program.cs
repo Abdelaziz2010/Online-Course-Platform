a5721e9065ebb897b546e4c067491f4e7b5492f4
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-06-03 01:09:54 +0300

    Refactor Program.cs for improved error handling
    Restructure code for better organization and clarity.
    Added a try-catch-finally block for application startup
    exception handling. Enhanced Azure AD B2C configuration
    for better token validation and logging. Reorganized
    middleware setup for improved error handling, logging,
    and security. Overall, these changes enhance the
    robustness and maintainability of the application.

--- EduPlatform.Presentation/Program.cs
+++ EduPlatform.Presentation/Program.cs
@@ -23,136 +23,146 @@ namespace EduPlatform.Presentation
             .Enrich.FromLogContext()
             .CreateBootstrapLogger();
 
-
-            #region Services Configuration
-
-            var builder = WebApplication.CreateBuilder(args);
-
-            var configuration = builder.Configuration;
-
-
-            // Add services to the container.
-
-            // Register Application Insights telemetry for monitoring and logging.
-            builder.Services.AddApplicationInsightsTelemetry();
-
-
-            builder.Host.UseSerilog((context, services, loggerConfiguration) => loggerConfiguration
-            .ReadFrom.Configuration(context.Configuration)
-            .ReadFrom.Services(services)
-            .WriteTo.Console(new ExpressionTemplate(
-                // Include trace and span ids when present.
-                "[{@t:HH:mm:ss} {@l:u3}{#if @tr is not null} ({substring(@tr,0,4)}:{substring(@sp,0,4)}){#end}] {@m}\n{@x}")));
-            
-            Log.Information("Starting the EduPlatform API.....");
-
-
-            #region Azure AD B2C configuration
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-              .AddMicrosoftIdentityWebApi(options =>
-              {
-                  configuration.Bind("AzureADB2C", options);
-
-                  options.Events = new JwtBearerEvents
-                  {
-                      OnTokenValidated = context =>
-                      {
-                          var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
-
-                          // Access the scope claim (scp) directly
-                          var scopeClaim = context.Principal?.Claims.FirstOrDefault(c => c.Type == "scp")?.Value;
-
-                          if (scopeClaim != null)
-                          {
-                              logger.LogInformation("Scope found in token: {Scope}", scopeClaim);
-                          }
-                          else
-                          {
-                              logger.LogWarning("Scope claim not found in token.");
-                          }
-
-                          return Task.CompletedTask;
-                      },
-                      OnAuthenticationFailed = context =>
-                      {
-                          var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
-                          logger.LogError("Authentication failed: {Message}", context.Exception.Message);
-                          return Task.CompletedTask;
-                      },
-                      OnChallenge = context =>
-                      {
-                          var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
-                          logger.LogError("Challenge error: {ErrorDescription}", context.ErrorDescription);
-                          return Task.CompletedTask;
-                      }
-                  };
-              }, options => { configuration.Bind("AzureADB2C", options); });
-
-            // The following flag can be used to get more descriptive errors in development environments
-
-            IdentityModelEventSource.ShowPII = true;
-
-            #endregion Azure AD B2C configuration
-
-
-            builder.Services.AddControllers();
-          
-            builder.Services.AddOpenApi();
-            
-            builder.Services.AddEndpointsApiExplorer();
-            
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddPresentationServices();
-
-            builder.Services.AddInfrastructureServices(configuration);
-
-            builder.Services.AddApplicationServices();
-
-            #endregion
-
-
-            #region Middlewares
-            
-            var app = builder.Build();
-
-            
-            // Global exception handling middleware
-            app.UseMiddleware<GlobalExceptionMiddleware>();
-
-            // Middlewares for logging request and response details.
-            app.UseMiddleware<RequestResponseLoggingMiddleware>();
-            app.UseMiddleware<RequestBodyLoggingMiddleware>();
-            app.UseMiddleware<ResponseBodyLoggingMiddleware>();
-
-
-            // Configure swagger middleware.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.MapOpenApi();
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                #region Services Configuration
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                var configuration = builder.Configuration;
+
+
+                // Add services to the container.
+
+                // Register Application Insights telemetry for monitoring and logging.
+                builder.Services.AddApplicationInsightsTelemetry();
+
+
+                builder.Host.UseSerilog((context, services, loggerConfiguration) => loggerConfiguration
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .WriteTo.Console(new ExpressionTemplate(
+                    // Include trace and span ids when present.
+                    "[{@t:HH:mm:ss} {@l:u3}{#if @tr is not null} ({substring(@tr,0,4)}:{substring(@sp,0,4)}){#end}] {@m}\n{@x}")));
+
+                Log.Information("Starting the EduPlatform API.....");
+
+
+                #region Azure AD B2C configuration
+
+                builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                  .AddMicrosoftIdentityWebApi(options =>
+                  {
+                      configuration.Bind("AzureADB2C", options);
+
+                      options.Events = new JwtBearerEvents
+                      {
+                          OnTokenValidated = context =>
+                          {
+                              var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+
+                              // Access the scope claim (scp) directly
+                              var scopeClaim = context.Principal?.Claims.FirstOrDefault(c => c.Type == "scp")?.Value;
+
+                              if (scopeClaim != null)
+                              {
+                                  logger.LogInformation("Scope found in token: {Scope}", scopeClaim);
+                              }
+                              else
+                              {
+                                  logger.LogWarning("Scope claim not found in token.");
+                              }
+
+                              return Task.CompletedTask;
+                          },
+                          OnAuthenticationFailed = context =>
+                          {
+                              var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+                              logger.LogError("Authentication failed: {Message}", context.Exception.Message);
+                              return Task.CompletedTask;
+                          },
+                          OnChallenge = context =>
+                          {
+                              var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+                              logger.LogError("Challenge error: {ErrorDescription}", context.ErrorDescription);
+                              return Task.CompletedTask;
+                          }
+                      };
+                  }, options => { configuration.Bind("AzureADB2C", options); });
+
+                // The following flag can be used to get more descriptive errors in development environments
+
+                IdentityModelEventSource.ShowPII = true;
+
+                #endregion Azure AD B2C configuration
+
+
+                builder.Services.AddControllers();
+
+                builder.Services.AddOpenApi();
+
+                builder.Services.AddEndpointsApiExplorer();
+
+                builder.Services.AddSwaggerGen();
+
+                builder.Services.AddPresentationServices();
+
+                builder.Services.AddInfrastructureServices(configuration);
+
+                builder.Services.AddApplicationServices();
+
+                #endregion
+
+
+                #region Middlewares
+
+                var app = builder.Build();
+
+
+                // Global exception handling middleware
+                app.UseMiddleware<GlobalExceptionMiddleware>();
+
+                // Middlewares for logging request and response details.
+                app.UseMiddleware<RequestResponseLoggingMiddleware>();
+                app.UseMiddleware<RequestBodyLoggingMiddleware>();
+                app.UseMiddleware<ResponseBodyLoggingMiddleware>();
+
+
+                // Configure swagger middleware.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.MapOpenApi();
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                #region  Azure AD B2C
+
+                app.UseAuthentication();
+
+                app.UseAuthorization();
+
+                #endregion  Azure AD B2C
+
+
+                app.UseMiddleware<SecurityHeadersMiddleware>();
+
+                app.MapControllers();
+
+                app.Run();
+
+                #endregion
             }
-            
-            app.UseHttpsRedirection();
-
-            #region  Azure AD B2C
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-            
-            #endregion  Azure AD B2C
-
-
-            app.UseMiddleware<SecurityHeadersMiddleware>();
-
-            app.MapControllers();
-
-            app.Run(); 
-
-            #endregion
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
