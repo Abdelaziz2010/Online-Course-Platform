b78183480132210e4587ca67d56ab1b201c1c641
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-06-02 14:42:57 +0300

    Add VideoRequestDTO and repository interface and implementation
    Introduce VideoRequestDTO for managing video request data with validation attributes.
    Define IVideoRequestRepository interface for CRUD operations on video requests.
    Implement VideoRequestRepository to interact with the database using Entity Framework Core.

---
+++ EduPlatform.Application/DTOs/VideoRequest/VideoRequestDTO.cs
@@ -0,0 +1,40 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace EduPlatform.Application.DTOs.VideoRequest
+{
+    public class VideoRequestDTO
+    {
+        public int VideoRequestId { get; set; }
+        
+        public int UserId { get; set; }
+        
+        public string? UserName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Topic { get; set; } = null!;
+
+        [Required]
+        [StringLength(50)]
+        public string SubTopic { get; set; } = null!;
+        
+        [Required]
+        [StringLength(50)]
+        public string RequestStatus { get; set; } = null!;
+        
+        [Required]
+        [StringLength(200)]
+        public string ShortTitle { get; set; } = null!;
+
+        [Required]
+        [StringLength(4000)]
+        public string RequestDescription { get; set; } = null!;
+        
+        [StringLength(4000)]
+        public string? Response { get; set; }
+        
+        [StringLength(2000)]
+        public string? VideoUrls { get; set; }
+    }
+}
---
+++ EduPlatform.Infrastructure/Implementations/Repositories/VideoRequestRepository.cs
@@ -0,0 +1,68 @@
+﻿
+using EduPlatform.Application.Interfaces.Repositories;
+using EduPlatform.Domain.Entities;
+using EduPlatform.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduPlatform.Infrastructure.Implementations.Repositories
+{
+    public class VideoRequestRepository : IVideoRequestRepository
+    {
+        private readonly EduPlatformDbContext _context;
+
+        public VideoRequestRepository(EduPlatformDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyList<VideoRequest?>> GetAllAsync()
+        {
+            return await _context.VideoRequests
+                .Include(vr => vr.User)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<VideoRequest?> GetByIdAsync(int id)
+        {
+            return await _context.VideoRequests
+                .Include(vr => vr.User)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(vr => vr.VideoRequestId == id);
+        }
+
+        public async Task<IReadOnlyList<VideoRequest?>> GetByUserIdAsync(int userId)
+        {
+            return await _context.VideoRequests
+                .Include(vr => vr.User)
+                .AsNoTracking()
+                .Where(vr => vr.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<VideoRequest> AddAsync(VideoRequest videoRequest)
+        {
+            _context.VideoRequests.Add(videoRequest);
+            await _context.SaveChangesAsync();
+            return videoRequest;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var videoRequest = await GetByIdAsync(id);
+           
+            if (videoRequest != null) 
+            {
+                _context.VideoRequests.Remove(videoRequest);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<VideoRequest> UpdateAsync(VideoRequest videoRequest)
+        {
+           _context.VideoRequests.Update(videoRequest);
+            await _context.SaveChangesAsync();
+            return videoRequest;
+        }
+    }
+}
