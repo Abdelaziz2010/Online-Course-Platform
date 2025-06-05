f15a341d4d897b0adb5222529c65f505468ffc4c
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-06-06 00:58:49 +0300

    Refactor course service and add instructor mapping
    Updated ICourseService to use IReadOnlyList for courses and added methods for course management and instructor retrieval. Enhanced CourseService with validation and session detail handling. Introduced InstructorMapping for DTO conversions using AutoMapper.

--- EduPlatform.Application/Interfaces/Services/ICourseService.cs
+++ EduPlatform.Application/Interfaces/Services/ICourseService.cs
@@ -4,7 +4,12 @@ namespace EduPlatform.Application.Interfaces.Services
 {
     public interface ICourseService
     {
-        Task<List<CourseDTO>> GetAllCoursesAsync(int? categoryId = null);
+        Task<IReadOnlyList<CourseDTO>> GetAllCoursesAsync(int? categoryId = null);
         Task<CourseDetailDTO> GetCourseDetailsByIdAsync(int courseId);
+        Task AddCourseAsync(CourseDetailDTO courseDTO); 
+        Task UpdateCourseAsync(CourseDetailDTO courseDTO);
+        Task DeleteCourseAsync(int courseId);
+        Task<IReadOnlyList<InstructorDTO>> GetAllInstructorsAsync();
+        Task<bool> UpdateCourseThumbnail(string courseThumbnailUrl, int courseId);
     }
 }
