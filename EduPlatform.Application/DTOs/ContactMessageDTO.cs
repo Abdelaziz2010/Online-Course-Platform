55a5a3e66d41ddba0beab3f91c13f389ea9f6a99
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-06-10 00:26:16 +0300

    create email functionality
    -create contact DTO
    -create IEmail interface and its implementation
    -create contact controller

---
+++ EduPlatform.Application/DTOs/ContactMessageDTO.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EduPlatform.Application.DTOs
+{
+    public record ContactMessageDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
