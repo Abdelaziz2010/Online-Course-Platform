a361c0d570e69ee5f6d9a2607fd77a6f495e28d4
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-06-10 21:19:56 +0300

    Add review management functionality
    Introduce IReviewRepository interface for review operations,
    implement ReviewRepository for database interactions,
    and add ReviewMapping for DTO mappings using AutoMapper.

---
+++ EduPlatform.Application/Mapping/ReviewMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EduPlatform.Application.DTOs.Course;
+using EduPlatform.Domain.Entities;
+
+namespace EduPlatform.Application.Mapping
+{
+    public class ReviewMapping : Profile
+    {
+        public ReviewMapping()
+        {
+            CreateMap<Review, ReviewDTO>()
+                .ForMember(dest => dest.UserName, op => op.MapFrom(src => $"{src.User.FirstName} {src.User.LastName}"));
+
+            CreateMap<ReviewDTO, Review>()
+                .ForMember(dest => dest.User, op => op.Ignore())
+                .ForMember(dest => dest.Course, op => op.Ignore());
+        }
+    }
+}
