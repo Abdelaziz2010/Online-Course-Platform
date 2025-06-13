5c5a7d68cf0f35729938149ae42906897e063b5d
abdelaziz <abdelazizsaleh1999@gmail.com>
2025-06-14 00:43:15 +0300

    Add PaymentsController for handling Stripe webhooks
    Introduces a new `PaymentsController` in the `EduPlatform.Presentation.Controllers` namespace. This controller processes Stripe webhook events through a POST endpoint, `HandleStripeWebhook`, utilizing an `IStripeService` dependency. It reads the request body and Stripe signature, and returns appropriate HTTP responses based on the success or failure of the webhook handling.

---
+++ EduPlatform.Presentation/Controllers/PaymentsController.cs
@@ -0,0 +1,36 @@
+﻿using EduPlatform.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduPlatform.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IStripeService _stripeService;
+
+        public PaymentsController(IStripeService stripeService)
+        {
+            _stripeService = stripeService;
+        }
+
+        [HttpPost("WebHook")]
+        public async Task<IActionResult> HandleStripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            var stripeSignature = Request.Headers["Stripe-Signature"];
+
+            try
+            {
+                await _stripeService.HandleWebhookAsync(json, stripeSignature);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
